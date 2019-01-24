a62649b207032be02ccc663714a71c01bc08e09c
Richard Feder <richardfederstaehle@college.harvard.edu>
2019-01-24 00:25:09 -0800

    Initial commit

---
+++ cgan.py
@@ -0,0 +1,209 @@
+import argparse
+import os
+import sys
+import numpy as np
+import math
+import time
+import cPickle as pickle
+
+
+from torch.autograd import Variable, grad
+import torch.nn as nn
+import torch
+
+import matplotlib
+matplotlib.use('tkAgg') 
+import matplotlib.pyplot as plt
+from helpers import *
+
+Device = 'cpu'
+
+nparam_dict = dict({'2d_gaussian':5, '1d_gaussian':2, 'bernoulli':2, 'ring':2, 'grid':2})
+outparam_dict = dict({'2d_gaussian':2, '1d_gaussian':1, 'bernoulli':1, 'ring':2, 'grid':2})
+lamD = 10.
+base_dir = '/Users/richardfeder/Documents/caltech/gan_work/results/'
+timestr = time.strftime("%Y%m%d-%H%M%S")
+# parse initial parameters, set to default if parameters left unspecified
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_iterations', type=int, default=1000, help='number of iterations in training')
+parser.add_argument('--batch_size', type=int, default=256, help='size of the batches')
+parser.add_argument('--n_hidden', type=int, default=512)
+parser.add_argument('--n_layers', type=int, default=3)
+parser.add_argument('--extraD', type=int, default=10)
+parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
+parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
+parser.add_argument('--b2', type=float, default=0.999, help='adam: decay of first order momentum of gradient')
+parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
+parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
+parser.add_argument('--loss_func', type=str, default='MSE', help='type of loss function used during training')
+parser.add_argument('--sample_type', type=str, default='1d_gaussian', help='type of distribution to learn')
+parser.add_argument('--verbosity', type=int, default=0, help='Verbosity level')
+parser.add_argument('--conditional_gan', type=int, default=1, help='1 to use conditional GAN, 0 otherwise')
+opt = parser.parse_args()
+print(opt)
+
+n_cond_params = nparam_dict[opt.sample_type] if opt.conditional_gan else 0
+
+cuda = True if torch.cuda.is_available() else False
+
+
+if opt.verbosity > 0:
+	print 'Number of conditional parameters:'
+	print n_cond_params
+	print 'Cuda: ', cuda
+
+
+def draw_true_samples(nsamp, samp_type='2d_gaussian', *argv):
+
+	'''For 2d_gaussian, *argv arguments are mu_0, mu_1, sig_1, sig_2, rho'''
+	if samp_type=='2d_gaussian':
+		mus = np.random.choice(np.linspace(-1, 1, 10), size=2)
+		sigs = np.random.choice(np.linspace(0.01, 2, 10), size=2)
+		rho = np.random.choice(np.linspace(-1, 1, 10))
+		
+		cov = np.array([[sigs[0]**2, rho*sigs[0]*sigs[1]],[rho*sigs[0]*sigs[1], sigs[1]**2]])
+		
+		conditional_params = np.column_stack((np.full(nsamp, mus[0]), np.full(nsamp, mus[1]), np.full(nsamp, sigs[0]), np.full(nsamp, sigs[1]), np.full(nsamp, rho)))
+		
+		s = torch.from_numpy(np.column_stack((np.random.multivariate_normal(mus, cov, nsamp), conditional_params)))
+		conditional_params = torch.from_numpy(conditional_params)
+
+		# print s
+	
+
+	elif samp_type=='1d_gaussian':
+		if len(argv)==0:
+			mu = np.random.choice(np.linspace(-1, 1, 10))
+			sig = np.random.choice(np.linspace(0.01, 2, 10))
+		else:
+			mu = argv[0]
+			sig = argv[1]
+
+		s = np.column_stack((np.random.normal(mu, sig, nsamp), np.full(nsamp, mu), np.full(nsamp, sig)))
+		conditional_params = np.column_stack((np.full(nsamp, mu), np.full(nsamp, sig)))
+		conditional_params = torch.from_numpy(conditional_params)
+
+
+		s = torch.from_numpy(s)
+	
+	elif samp_type=='bernoulli':
+		n = argv[0]
+		p = argv[1]
+
+		conditional_params = torch.cat((n,p), 0)
+		s = torch.from_numpy(np.random.binomial(n, p, nsamp))
+
+	elif samp_type == 'ring':
+		k = argv[0]
+		std = argv[1]
+		m = means_circle(k)
+		i = torch.zeros(nsamp).random_(m.size(0)).long()
+
+		conditional_params = torch.cat((k, std), 0)
+		s = torch.randn(nsamp, 2) * std + m[i]
+	
+	elif samp_type == 'grid':
+		k = argv[0]
+		std = argv[1]
+		m = means_grid(k)
+		i = torch.zeros(nsamp).random_(m.size(0)).long()
+		conditional_params = torch.cat((k,std), 0)
+		s = torch.randn(nsamp, 2) * std + m[i]
+
+	return s.float(), conditional_params.float()
+
+
+
+def sample_noise(bs, d):
+	z = torch.randn(bs, d).float()
+	return Variable(z.to(Device), requires_grad=True)
+
+
+def main():
+	'''Initialize the discriminator and generator from the Perceptron class, use Adam optimizer'''
+
+	# take in (x,y) position along with conditional params, return softmax output
+	netD = Perceptron([outparam_dict[opt.sample_type]+n_cond_params] + [opt.n_hidden] * opt.n_layers + [1]) 
+	# take in latent vector and generate (x,y) position
+	netG = Perceptron([opt.latent_dim+n_cond_params] + [opt.n_hidden] * opt.n_layers + [outparam_dict[opt.sample_type]])
+	
+	netD.to(Device)
+	netG.to(Device)
+	optD = torch.optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+	optG = torch.optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+	lossD_vals, lossG_vals = [],[]
+
+	# wasserstein gan objective for now
+	objective = objective_wgan
+
+	# ----------
+	#  Training
+	# ----------
+
+	for i in xrange(opt.n_iterations):
+
+		# training for discriminator with opt.extraD updates per one generator update
+		for j in xrange(opt.extraD+1):
+			
+			real, cond_params = draw_true_samples(opt.batch_size, samp_type=opt.sample_type)
+
+			if opt.verbosity > 1:
+				print 'conditional parameters:', real[0, 1:]
+
+			gen_input = sample_noise(opt.batch_size, opt.latent_dim)
+			
+			if cond_params is not None:
+
+				gen_input = torch.cat((gen_input, cond_params), 1)
+				if opt.verbosity > 1:
+					print 'gen_input has shape', gen_input.shape
+			
+			fake = netG(gen_input)
+			
+			if opt.verbosity > 1:
+				print 'fake has shape', fake.shape
+
+			fake = torch.cat((fake, cond_params), 1)
+
+			if j < opt.extraD:
+				optD.zero_grad()
+				lossD = objective(netD(real), netD(fake)) # calculate the loss function
+				gradD = grad(lossD * opt.batch_size, fake, create_graph=True)[0] 
+				reguD = gradD.norm(2, 1).clamp(1).mean()
+				(lossD + lamD * reguD).backward() # backward prop with some regularization
+				optD.step()
+			else:
+				optG.zero_grad()
+				lossG = - objective(netD(real), netD(fake))
+				(lossG).backward()
+				optG.step()
+ 		
+
+		# save losses from the last iteration
+		lossD_vals.append(-1*lossD.item())
+		lossG_vals.append(lossG.item())
+
+		if (i % int(opt.n_iterations/10))==0:
+			print 'Iteration', i, 'lossD =', np.round(lossD.item(), 6), 'lossG =', np.round(lossG.item(), 6), 'reguD =', np.round(reguD.item(), 6)
+
+		if opt.verbosity > 1:
+			print 'lossD'
+			print -1*lossD.item()
+			print 'lossG'
+			print lossG.item()
+    
+
+	new_dir, frame_dir = create_directories(timestr)
+	
+	plot_loss_iterations(lossD_vals, lossG_vals, new_dir)
+
+	save_nn(netG, new_dir+'/netG')
+
+	save_params(new_dir, opt.sample_type)
+
+
+# execute program     
+main()
+
+
---
+++ helpers.py
@@ -0,0 +1,97 @@
+import argparse
+import os
+import sys
+import numpy as np
+import math
+import time
+import cPickle as pickle
+
+from torch.autograd import Variable, grad
+import torch.nn as nn
+import torch
+
+import matplotlib
+matplotlib.use('tkAgg') 
+import matplotlib.pyplot as plt
+
+nparam_dict = dict({'2d_gaussian':5, '1d_gaussian':2, 'bernoulli':2, 'ring':2, 'grid':2})
+outparam_dict = dict({'2d_gaussian':2, '1d_gaussian':1, 'bernoulli':1, 'ring':2, 'grid':2})
+
+def create_directories(time_string):
+    new_dir = base_dir+time_string
+    if not os.path.isdir(new_dir):
+        os.makedirs(new_dir)
+    frame_dir = new_dir+'/frames'
+    if not os.path.isdir(frame_dir):
+        os.makedirs(frame_dir)
+    print 'New directories:'
+    print new_dir
+    print frame_dir
+    return new_dir, frame_dir
+
+
+def objective_wgan(fakeD, realD):
+    return fakeD.mean() - realD.mean()
+
+def save_nn(model, path):
+    torch.save(model.state_dict(), path)
+
+def save_params(dir, sample_type):
+    # save parameters as dictionary, then pickle them to txt file
+    param_dict = vars(opt)
+    print param_dict
+    param_dict['n_cond_params'] = n_cond_params
+    param_dict['n_out'] = outparam_dict[sample_type]
+    with open(dir+'/params.txt', 'w') as file:
+        file.write(pickle.dumps(param_dict))
+
+def init_loss(loss_func):
+    # Loss functions
+    if loss_func=='MSE':
+        adversarial_loss = torch.nn.MSELoss()
+    elif loss_func=='KL':
+        adversarial_loss = torch.nn.KLDivLoss()
+    elif loss_func=='SmoothL1':
+        adversarial_loss = torch.nn.SmoothL1Loss()
+    elif loss_func=='L1':
+        adversarial_loss = torch.nn.l1_loss()
+    return adversarial_loss
+
+
+def plot_loss_iterations(lossD_vals, lossG_vals, directory):
+    n = len(lossD_vals)
+
+    plt.figure(figsize=(8,4))
+    plt.subplot(1,2,1)
+    plt.plot(list(torch.linspace(1, n, steps=n)), lossD_vals, label='Discriminator')
+    plt.legend()
+    plt.yscale('log')
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss * -1')
+    plt.subplot(1,2,2)
+    plt.plot(list(torch.linspace(1, n, steps=n)), lossG_vals, label='Generator')
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.yscale('log')
+    plt.savefig(directory+'/loss_GD.png', bbox_inches='tight')
+    plt.close()
+
+
+
+class Perceptron(torch.nn.Module):
+    def __init__(self, sizes, final=None):
+        super(Perceptron, self).__init__() # what does this line do?
+        layers = []
+        print 'Initializing Neural Net'
+        for i in xrange(len(sizes) - 1):
+            print 'Layer', i, ':', sizes[i], sizes[i+1]
+            layers.append(torch.nn.Linear(sizes[i], sizes[i + 1]))
+            if i != (len(sizes) - 2):
+                layers.append(torch.nn.ReLU())
+        if final is not None:
+            layers.append(final())
+        self.net = torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x)
